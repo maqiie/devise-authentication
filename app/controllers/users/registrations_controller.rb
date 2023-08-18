2aacd66dd9556636ab0eca3432614e251beb42ca
maqiie <99504010+maqiie@users.noreply.github.com>
2023-08-18 03:41:25 +0300

    initial commit

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  # def create
+  #   super
+  # end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+  body: JSON.stringify({user:{
+    email:"example@gmail.com",
+    password:"example_password"
+    }})
+  
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def render_resource(resource)
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: 'Signed up successfully.' },
+        data: {
+          id: resource.id,
+          email: resource.email
+          # Include other attributes you want to expose
+        }
+      }
+    else
+      render json: {
+        status: { code: 422, message: 'Sign up failed.' },
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+end
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+  body: JSON.stringify({user:{
+    email:"example@gmail.com",
+    password:"example_password",
+    password_confirmation:"<PASSWORD>"
+    }})
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+  private
+
+def sign_in_params
+  params.require(:user).permit(:email, :password, :password_confirmation)
+end
+
+
+def respond_with(resource, _opts = {})
+  render json: {
+    status: { code: 200, message: 'Logged in successfully.' },
+    data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+  }, status: :ok
+end
+
+def respond_to_on_destroy
+  if current_user
+    render json: {
+      status: 200,
+      message: "Logged out successfully"
+    }, status: :ok
+  else
+    render json: {
+      status: 401,
+      message: "Couldn't find an active session."
+    }, status: :unauthorized
+  end
+end
+end
